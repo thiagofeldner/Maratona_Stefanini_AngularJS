5deddb89c887b4da64148de31183aaf57ac5c040
thiagofeldner <tfeldner@hotmail.com>
2020-10-07 13:12:33 -0300

    Segundo commit da maratona stefanini separados por pastas e identificados pelo dia, por enquanto só estou subindo os exemplos que são executados durante as aulas.

---
+++ dia-2/lista-tarefas.js
@@ -0,0 +1,55 @@
+(function () {
+
+    "use strict";
+
+    //Module
+    angular.module('listaTarefasApp', []);
+
+    //Controller
+    angular.module('listaTarefasApp')
+        .controller('listaTarefasController', listaTarefasController);
+
+    listaTarefasController.$inject = ['$scope'];
+
+    function listaTarefasController ($scope) {
+        var vm = this;
+        
+        vm.tarefas = [
+            { text : 'Estudar AngularJs', feito:true},
+            { text : 'Fazer uma aplicação em AngularJs', feito:false}
+        ];       
+        
+        vm.restam = restam;
+        vm.addTarefa = addTarefa;
+        vm.arquivar = arquivar;
+        
+        function restam(){
+            var count = 0;
+            angular.forEach(vm.tarefas, function(trf) {
+                if (!trf.feito) count++;
+            });
+            return count;
+        }
+
+        function addTarefa(){
+            vm.tarefas.push({ text: vm.tarefaText, feito: false });
+            vm.tarefaText = '';
+        }
+
+
+        function arquivar(){
+            // var oldTarefas = vm.tarefas;
+            // vm.tarefas = [];
+            // angular.forEach(oldTarefas, function(trf){
+            //     if(!trf.feito) 
+            //         vm.tarefas.push(trf);
+            // });
+
+            vm.tarefas = vm.tarefas.filter(function(trf) {
+                return !trf.feito
+            });
+        }
+    }
+
+})();
+
